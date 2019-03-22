e7047bb8d78aa5a163b501d676fc613069950d5b
mrfabroa <eric.fabroa@ycdsbk12.ca>
2019-03-22 09:39:08 -0400

    coin solution

--- Coin.py
+++ Coin.py
@@ -1,7 +1,61 @@
-# Define your Coin class here
+"""
+-------------------------------------------------------------------------------
+Name:		Coin.py
+Purpose:
+Simulates a coin flip
+
+Author:		Leung.G
+
+Created:		26/11/2018
+------------------------------------------------------------------------------
+"""
+
+import random
+
+class Coin(object):
+    """
+    Models a coin as an object
+    """
+
+    def __init__(self):
+        """
+        Initializes the face of the coin to either heads or tails
+        :return: None
+        """
+
+        self.face = random.choice(["heads", "tails"])
 
 
+    def get_face(self):
+        """
+        Retrieves the current face of the coin facing up
+        :return: string The value of the face attribute
+        """
+        return self.face
+
+    def flip(self):
+        """
+        Sets the face attribute to heads or tails
+        :return: None
+        """
+        self.face = random.choice(["heads", "tails"])
+
+
+
+# Simulates 1000 coin flips and saves the data to accumulator variables
 if __name__ == '__main__':
-  # put your code that utilizes your Coin class here
-  pass
-  
+
+    # initialize the counters
+    heads = 0
+    tails = 0
+
+    for i in range(1000):
+        myCoin = Coin()
+        myCoin.flip()
+
+        if myCoin.get_face() == "heads":
+            heads += 1
+        else:
+            tails += 1
+
+    print("Total Heads: " + str(heads) + "\nTotal Tails: " + str(tails))
